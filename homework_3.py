eb1c89d9eac410559c11ef0651f27623e69dacb2
susannefuerst <susannefuerst@freenet.de>
2018-05-29 13:06:14 +0200

    completed homework 3

--- homework_3.py
+++ homework_3.py
@@ -3,6 +3,15 @@ def average(args):
 
 def median(args):
 	length = len(args)
-	sort(args)
-	if length % 2 == 1: return (args[(length + 1)/2])
-	else: return (args[(length/2)] + args[length/2 + 1])/2
+	sortedList = sorted(args);
+	if length % 2 == 1:
+		return sortedList[int((length - 1)/2)]
+	else: 
+		return (sortedList[int(length/2)] + sortedList[int(length/2 - 1)])/2
+	
+if __name__ == "__main__":
+	assert average([5,5,5,5,5]) == 5
+	assert average([5,10,5,20]) == 10
+	assert median([5,10,5,20]) == 7.5
+	assert median([5,3,1,9,10]) == 5
+	print('passed the test')	
